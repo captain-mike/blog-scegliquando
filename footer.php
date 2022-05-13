66b6ab82d456da1ba091be2c91a77ae7eeb4eb84
captain-mike <info@grafikweb.it>
2022-05-13 09:00:27 +0200

    first settings

--- footer.php
+++ footer.php
@@ -1,11 +1,15 @@
 <?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'scegliquando' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div class="container">
+
+        <div id="copyright">
+            &copy; <?php echo esc_html(date_i18n(__('Y', 'scegliquando'))); ?> <?php echo esc_html(get_bloginfo('name')); ?>
+        </div>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php get_header(); ?>
-<main id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</main>
+<div class="row">
+
+</div>
+<div class="row">
+
+    <main id="content" role="main">
+        <div class="row">
+
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="col-12 col-md-6 col-lg-4">
+
+                <div class="post-image">
+                    <?php the_post_thumbnail();?>
+                </div>
+                <div class="post-preview">
+                    <h4><?php the_title() ?></h4>
+                    <div><?php the_date() ?></div>
+                    <div><?php the_excerpt() ?></div>
+                </div>
+                
+                
+            </div>
+            <?php endwhile;
+        endif; ?>
+        </div>
+        <?php get_template_part('nav', 'below'); ?>
+    </main>
+</div>
 <?php get_footer(); ?>
