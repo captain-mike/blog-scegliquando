977e67a1f933450f23c616625cae52f37d1e10f5
captain-mike <info@grafikweb.it>
2022-06-02 12:23:19 +0200

    single post

--- single.php
+++ single.php
@@ -1,11 +1,23 @@
 <?php get_header(); ?>
 <main id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
-<footer class="footer">
-<?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
+    <div class="container my-5">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <h1 class="post-title mb-5"><?php the_title(); ?></h1>
+            <div id="post-content">
+                <div class="post-thumb">
+                    <?php the_post_thumbnail();?>
+                </div>
+                <?php the_content() ?>
+                <hr class="clearfix">
+            </div>
+
+        <?php endwhile;
+        endif; ?>
+        <footer class="footer">
+            <?php get_template_part('nav', 'below-single'); ?>
+        </footer>
+    </div>
 </main>
 <?php get_footer(); ?>
